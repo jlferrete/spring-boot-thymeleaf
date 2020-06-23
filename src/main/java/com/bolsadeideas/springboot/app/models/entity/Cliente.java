3d583e76bb3d90d7a1b8107c4dfad5f99b33c81b
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-23 13:23:49 +0200

    Added Facturacion

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -27,10 +32,10 @@ public class Cliente implements Serializable {
 
 	@NotEmpty
 	private String nombre;
-	
+
 	@NotEmpty
 	private String apellido;
-	
+
 	@NotEmpty
 	@Email
 	private String email;
@@ -38,11 +43,18 @@ public class Cliente implements Serializable {
 	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Factura> facturas;
+
 	private String foto;
-	
+
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -82,7 +94,7 @@ public class Cliente implements Serializable {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-	
+
 	public String getFoto() {
 		return foto;
 	}
@@ -91,10 +103,26 @@ public class Cliente implements Serializable {
 		this.foto = foto;
 	}
 
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
+	
+	@Override
+	public String toString() {
+		return nombre + " " + apellido;
+	}
+	
 	private static final long serialVersionUID = 1L;
-
 }
--- src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
@@ -9,14 +9,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.dao.IFacturaDao;
+import com.bolsadeideas.springboot.app.models.dao.IProductoDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.entity.Factura;
+import com.bolsadeideas.springboot.app.models.entity.Producto;
 
 @Service
 public class ClienteServiceImpl implements IClienteService {
 
 	@Autowired
 	private IClienteDao clienteDao;
-	
+
+	@Autowired
+	private IProductoDao productoDao;
+
+	@Autowired
+	private IFacturaDao facturaDao;
+
 	@Override
 	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
@@ -28,7 +38,7 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional
 	public void save(Cliente cliente) {
 		clienteDao.save(cliente);
-		
+
 	}
 
 	@Override
@@ -42,12 +52,44 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional
 	public void delete(Long id) {
 		clienteDao.deleteById(id);
-		
+
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Page<Cliente> findAll(Pageable pageable) {
 		return clienteDao.findAll(pageable);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Producto> findByNombre(String term) {
+		return productoDao.findByNombreLikeIgnoreCase("%" + term + "%");
+	}
+
+	@Override
+	@Transactional
+	public void saveFactura(Factura factura) {
+		facturaDao.save(factura);
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Producto findProductoById(Long id) {
+		// TODO Auto-generated method stub
+		return productoDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Factura findFacturaById(Long id) {
+		// TODO Auto-generated method stub
+		return facturaDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public void deleteFactura(Long id) {
+		facturaDao.deleteById(id);
 	}
 }
